356060dba8656636462e686043ebd9ea949290d0
Mario <mariovladimirovfs@gmail.com>
2022-04-21 16:44:38 +0300

    Triangle of 55 Stars

---
+++ 01. JavaScript Book/1 First Steps In Coding/04. triangleOf55Stars.js
@@ -0,0 +1,21 @@
+function triangleOf55Stars() {
+  let n = 10;
+  let output = ``;
+
+  for (i = 1; i <= n; i++) {
+    let i = `*`;
+    output += `${i}`;
+    console.log(output);
+  }
+}
+// console.log(`*`);
+// console.log(`**`);
+// console.log(`***`);
+// console.log(`****`);
+// console.log(`*****`);
+// console.log(`******`);
+// console.log(`*******`);
+// console.log(`********`);
+// console.log(`*********`);
+// console.log(`**********`);
+triangleOf55Stars();
