ada3f9e62c35a257ae6d67ac14f7c96ba2cbfa36
Mario <mariovladimirovfs@gmail.com>
2022-05-12 07:25:31 +0300

    upload

---
+++ 01. JavaScript Book/4.1. Complex Conditions/13. Point in the Figure.js
@@ -0,0 +1,32 @@
+function pointInTheFigure(input) {
+
+    let h = Number(input[0]);
+    let x = Number(input[1]);
+    let y = Number(input[2]);
+
+
+
+    let outRectangle1 = (x < 0 || x > 3 * h) || (y < 0 || y > h) 
+    let outRectangle2 = (x < h || x > 2 * h) || (y < h || y > 4 * h) 
+
+    let inRectangle1 = (x > 0 && x < 3 * h) && (y > 0 && y < h)
+    let inRectangle2 = (x > h && x < 2 * h) && (y >= h && y < 4 * h)
+
+    let commonBorder = x > h && x < 2 * h && y === h
+
+    if (outRectangle1 && outRectangle2) {
+        console.log(`outside`);
+    } else if (inRectangle1 || inRectangle2 || commonBorder) {
+        console.log(`inside`);
+    } else {
+        console.log(`border`);
+    }
+}
+pointInTheFigure([`2`, `3`, `10`]); //outside
+pointInTheFigure([`2`, `3`, `1`])  //inside
+pointInTheFigure([`2`, `2`, `2`]) //border
+pointInTheFigure([`2`, `6`, `0`]) //border
+pointInTheFigure([`2`, `0`, `6`]) //outside
+pointInTheFigure([`15`, `13`, `55`]) //outside
+pointInTheFigure([`15`, `29`, `37`]) //inside
+pointInTheFigure([`15`, `37`, `18`]) //outside
