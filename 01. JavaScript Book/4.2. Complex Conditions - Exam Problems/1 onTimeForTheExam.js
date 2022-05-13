8a8b0e41aee1fa2896d8869a7c2eaef8b5f36dbd
Mario <mariovladimirovfs@gmail.com>
2022-05-13 09:00:34 +0300

    update

--- 01. JavaScript Book/4.2. Complex Conditions - Exam Problems/1 onTimeForTheExam.js
+++ 01. JavaScript Book/4.2. Complex Conditions - Exam Problems/1 onTimeForTheExam.js
@@ -39,10 +39,12 @@ function onTimeForTheExam(input) {
   }
 }
 onTimeForTheExam([`9`, `30`, `9`, `50`]);
-onTimeForTheExam([`9`, `00`, `10`, `30`]);
-onTimeForTheExam([`9`, `00`, `10`, `5`]);
-onTimeForTheExam([`10`, `00`, `10`, `00`]);
-onTimeForTheExam([`9`, `00`, `8`, `30`]);
 onTimeForTheExam([`16`, `00`, `15`, `00`]);
-onTimeForTheExam([`11`, `30`, `10`, `55`]);
+onTimeForTheExam([`9`, `00`, `8`, `30`]);
+onTimeForTheExam([`9`, `00`, `10`, `30`]);
 onTimeForTheExam([`14`, `00`, `13`, `55`]);
+onTimeForTheExam([`11`, `30`, `8`, `12`]);
+onTimeForTheExam([`10`, `00`, `10`, `00`]);
+onTimeForTheExam([`11`, `30`, `10`, `55`]);
+onTimeForTheExam([`11`, `30`, `12`, `29`]);
+
