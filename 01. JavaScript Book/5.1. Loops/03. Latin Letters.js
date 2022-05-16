ef3d14a46168a6e09d05ac4761b7c03b16954fbb
Mario <mariovladimirovfs@gmail.com>
2022-05-16 15:11:05 +0300

    upload

---
+++ 01. JavaScript Book/5.1. Loops/03. Latin Letters.js
@@ -0,0 +1,13 @@
+function latinLetters() {
+  for (let i = 97; i <= 122; i++) {
+    console.log(String.fromCharCode(i));
+  }
+}
+latinLetters();
+
+// function latinLetters2() {
+//   for (let ch = `a`.charCodeAt(0); ch <= `z`.charCodeAt(0); ch++) {
+//     console.log(String.fromCharCode(ch));
+//   }
+// }
+// latinLetters2();
