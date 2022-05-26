63e8c1175515678a9545c0cc1f4c97320daeee99
Mario <mariovladimirovfs@gmail.com>
2022-05-26 20:37:12 +0300

    upload

---
+++ 01. JavaScript Book/6.1. Drawing on the Console/06. Rhombus of Stars.js
@@ -0,0 +1,27 @@
+function rhombusOfStars(input) {
+  let num = Number(input[0]);
+
+  for (let row = 1; row <= num; row++) {
+    let line = ``;
+    for (let col = 1; col <= num - row; col++) {
+      line += ` `;
+    }
+    line += `*`;
+
+    for (let col = 1; col < row; col++) {
+      line += ` *`;
+    }
+    console.log(line);
+  }
+  for (let row = 1; row <= num - 1; row++) {
+    line = ``;
+    for (let col = 1; col < row; col++) {
+      line += ` `;
+    }
+    for (let col = 1; col <= num - row; col++) {
+      line += ` *`;
+    }
+    console.log(line);
+  }
+}
+rhombusOfStars([`6`]);
