598f7e3cc2148d8f320970119b1176df95622008
Mario <mariovladimirovfs@gmail.com>
2022-05-27 17:56:51 +0300

    upload

--- 01. JavaScript Book/6.1. Drawing on the Console/08. Sunglasses.js
+++ 01. JavaScript Book/6.1. Drawing on the Console/08. Sunglasses.js
@@ -1,43 +1,38 @@
 function sunGlasses(input) {
+  let num = Number(input[0]);
 
-    let num = Number(input[0]);
+  let topLine = `*`.repeat(num * 2);
+  topLine += ` `.repeat(num);
+  topLine += `*`.repeat(num * 2);
+  console.log(topLine);
 
-    let topLine = `*`.repeat(num * 2);
-    topLine += ` `.repeat(num);
-    topLine += `*`.repeat(num * 2);
-    console.log(topLine);
+  let middleFrame = 0;
 
-    let middleFrame = 0;
+  if (num % 2 == 0) {
+    middleFrame = Math.floor(num / 2 - 1);
+  } else {
+    middleFrame = Math.ceil(num / 2 - 1);
+  }
 
-    if (num % 2 == 0) {
-        middleFrame = Math.floor((num / 2) - 1);
+  for (let i = 1; i <= num - 2; i++) {
+    let middleLine = ``;
+    if (i === middleFrame) {
+      middleLine += `|`.repeat(num);
     } else {
-        middleFrame = Math.ceil((num / 2) - 1);
+      middleLine += ` `.repeat(num);
     }
 
-    for (let i = 1; i <= num - 2; i++) {
-       
-        let middleLine = ``;
-        if (i === middleFrame) {
-            middleLine += `|`.repeat(num);
-        } else {
-            middleLine += ` `.repeat(num);
-        }
-  
-        
-        let frame = `*`;
-        for (let j = 1; j <= 2 * num - 2; j++) {
-            frame += `/`;
-        }
-        frame += `*`;
-        console.log(frame + middleLine + frame);
-
+    let frame = `*`;
+    for (let j = 1; j <= 2 * num - 2; j++) {
+      frame += `/`;
     }
-    let bottomLine = `*`.repeat(num * 2);
-    bottomLine += ` `.repeat(num);
-    bottomLine += `*`.repeat(num * 2);
+    frame += `*`;
+    console.log(frame + middleLine + frame);
+  }
+  let bottomLine = `*`.repeat(num * 2);
+  bottomLine += ` `.repeat(num);
+  bottomLine += `*`.repeat(num * 2);
 
-    console.log(bottomLine);
+  console.log(bottomLine);
 }
-sunGlasses([`5`])
-
+sunGlasses([`5`]);
