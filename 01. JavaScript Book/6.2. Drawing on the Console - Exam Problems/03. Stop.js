ddd813ea5b77116e92158ca2a780783074fbb5ca
Mario <mariovladimirovfs@gmail.com>
2022-05-31 08:36:01 +0300

    upload

---
+++ 01. JavaScript Book/6.2. Drawing on the Console - Exam Problems/03. Stop.js
@@ -0,0 +1,50 @@
+function stop(input) {
+  let num = Number(input[0]);
+
+  let topPart = num + 2;
+  let downPart = num;
+
+  let outerDots = ``;
+
+  let firstLineRepeat = 2 * num + 1;
+  let middleFirstLine = ``;
+
+  let middleLineRepeat = 2 * num + 1 - 2;
+  let middle = ``;
+
+  let start = ``;
+  let end = ``;
+
+  for (top = 1; top <= topPart - 1; top++) {
+    outerDots = `.`.repeat(topPart - top);
+    start = `//`;
+    middle = `_`.repeat(middleLineRepeat);
+    end = `\\\\`;
+    if (top === 1) {
+      middleFirstLine = `_`.repeat(firstLineRepeat);
+      console.log(outerDots + middleFirstLine + outerDots);
+    } else {
+      console.log(outerDots + start + middle + end + outerDots);
+      middleLineRepeat += 2;
+    }
+  }
+  let middleRepeat = 2 * num + 1 - 4;
+  let middleForstop = `_`.repeat(middleRepeat);
+  let middlePart = `${start}${middleForstop}STOP!${middleForstop}${end}`;
+  console.log(middlePart);
+
+  for (let down = 0; down < downPart; down++) {
+    outerDots = `.`.repeat(down);
+    let endForDown = `//`;
+    let middleDownPart = `_`.repeat(middleLineRepeat);
+    let start = `\\\\`;
+
+    if (down === num - 1) {
+      console.log(outerDots + start + middleDownPart + endForDown + outerDots);
+    } else {
+      console.log(outerDots + start + middleDownPart + endForDown + outerDots);
+    }
+    middleLineRepeat -= 2;
+  }
+}
+stop([`7`]);
