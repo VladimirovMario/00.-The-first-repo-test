c56960b52e3f39d4eedb7e27661ae8934526149f
Mario <103949296+VladimirovMario@users.noreply.github.com>
2022-06-06 10:32:38 +0300

    Add files via upload

---
+++ 01. JavaScript Book/7.1. Complex Loops/15. Number Table.js
@@ -0,0 +1,18 @@
+function numberTable(input) {
+  let num = Number(input[0]);
+  let result = ``;
+
+  for (let row = 0; row < num; row++) {
+    for (let col = 0; col < num; col++) {
+      let currentNum = row + col + 1;
+
+      if (currentNum > num) {
+        currentNum = 2 * num - currentNum;
+      }
+      result = result + currentNum + ` `;
+    }
+    console.log(result);
+    result = ``;
+  }
+}
+numberTable([`6`]);
