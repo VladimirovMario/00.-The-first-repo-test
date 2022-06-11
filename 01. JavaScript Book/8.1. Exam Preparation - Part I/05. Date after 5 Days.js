edd9abd3188de3042863128d62e86438e39b6903
Mario <mariovladimirovfs@gmail.com>
2022-06-11 09:27:21 +0300

    upload

--- 01. JavaScript Book/8.1. Exam Preparation - Part I/05. Date after 5 Days.js
+++ 01. JavaScript Book/8.1. Exam Preparation - Part I/05. Date after 5 Days.js
@@ -1,35 +1,33 @@
 function dateAfter5Days(input) {
+  let day = Number(input[0]);
+  let month = Number(input[1]);
 
-    let day = Number(input[0]);
-    let month = Number(input[1]);
+  let daysOfMonth = 0;
 
-    let daysOfMonth = 0;
+  if (month === 4 || month === 6 || month === 9 || month === 11) {
+    daysOfMonth = 30;
+  } else if (month === 2) {
+    daysOfMonth = 28;
+  } else {
+    daysOfMonth = 31;
+  }
+  day += 5;
 
-    if (month === 4 || month === 6 || month === 9 || month === 11) {
-        daysOfMonth = 30;
-    } else if (month === 2) {
-        daysOfMonth = 28;
-    } else {
-        daysOfMonth = 31;
+  if (day > daysOfMonth) {
+    month++;
+    day -= daysOfMonth;
+    if (month > 12) {
+      month = 1;
     }
-    day += 5;
+  }
 
-    if (day > daysOfMonth) {
-        month++;
-        day -= daysOfMonth;
-        if (month > 12) {
-            month = 1;
-        }
-    }
-
-    if (month < 10) {
-        console.log(`${day}.0${month}`);
-    } else {
-        console.log(`${day}.${month}`);
-    }
-
+  if (month < 10) {
+    console.log(`${day}.0${month}`);
+  } else {
+    console.log(`${day}.${month}`);
+  }
 }
 dateAfter5Days([`28`, `03`]);
-dateAfter5Days([`27`, `12`])
-dateAfter5Days([`25`, `1`])
-dateAfter5Days([`26`, `02`])
+dateAfter5Days([`27`, `12`]);
+dateAfter5Days([`25`, `1`]);
+dateAfter5Days([`26`, `02`]);
