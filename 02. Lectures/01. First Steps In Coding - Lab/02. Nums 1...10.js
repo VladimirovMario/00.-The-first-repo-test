3681909042410b806f54f192eb6c400099e2aae9
Mario <mariovladimirovfs@gmail.com>
2022-06-18 10:31:46 +0300

    upload

---
+++ 02. Lectures/01. First Steps In Coding - Lab/02. Nums 1...10.js
@@ -0,0 +1,13 @@
+function numbersFrom1to10() {
+  console.log(Number`1`);
+  console.log(Number`2`);
+  console.log(Number`3`);
+  console.log(Number`4`);
+  console.log(Number`5`);
+  console.log(Number`6`);
+  console.log(Number`7`);
+  console.log(Number`8`);
+  console.log(Number`9`);
+  console.log(Number`10`);
+}
+numbersFrom1to10()
---
+++ 02. Lectures/01. First Steps In Coding - Lab/03. Square Area.js
@@ -0,0 +1,6 @@
+function square(input) {
+  let a = Number(input[0]);
+  let equal = a * a;
+  console.log(equal);
+}
+square([5]);
