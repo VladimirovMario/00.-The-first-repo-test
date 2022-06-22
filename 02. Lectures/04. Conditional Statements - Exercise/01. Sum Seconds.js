60e9eef6624eb90e0901b826843952dddb803326
Mario <mariovladimirovfs@gmail.com>
2022-06-22 09:26:19 +0300

    upload

---
+++ 02. Lectures/04. Conditional Statements - Exercise/01. Sum Seconds.js
@@ -0,0 +1,18 @@
+function sumSeconds(input) {
+  let time1 = Number(input[0]);
+  let time2 = Number(input[1]);
+  let time3 = Number(input[2]);
+
+  let sumTime = time1 + time2 + time3;
+  let minutes = Math.floor(sumTime / 60);
+  let seconds = sumTime % 60;
+
+  if (seconds < 10) {
+    console.log(`${minutes}:0${seconds}`);
+  } else {
+    console.log(`${minutes}:${seconds}`);
+  }
+}
+sumSeconds(["14", "12", "10"]);
+sumSeconds(["22", "7", "34"]);
+sumSeconds(["50", "50", "49"]);
