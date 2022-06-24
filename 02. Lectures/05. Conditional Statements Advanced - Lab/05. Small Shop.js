73b8ebb29b6fb466e734e4e61abfc25c865dd335
Mario <mariovladimirovfs@gmail.com>
2022-06-24 20:39:42 +0300

    upload

---
+++ 02. Lectures/05. Conditional Statements Advanced - Lab/05. Small Shop.js
@@ -0,0 +1,62 @@
+function smallShop(input) {
+    let product = (input[0]);
+    let town = (input[1]);
+    let count = Number(input[2]);
+
+    if (town === `Sofia`) {
+        switch (product) {
+            case `coffee`:
+                console.log(`${count}` * 0.50);
+                break;
+            case `water`:
+                console.log(`${count}` * 0.80);
+                break;
+            case `beer`:
+                console.log(`${count}` * 1.20);
+                break;
+            case `sweets`:
+                console.log(`${count}` * 1.45);
+                break;
+            case `peanuts`:
+                console.log(`${count}` * 1.60);
+                break;
+        }
+    } else if (town === `Plovdiv`) {
+        switch (product) {
+            case `coffee`:
+                console.log(`${count}` * 0.40);
+                break;
+            case `water`:
+                console.log(`${count}` * 0.70);
+                break;
+            case `beer`:
+                console.log(`${count}` * 1.15);
+                break;
+            case `sweets`:
+                console.log(`${count}` * 1.30);
+                break;
+            case `peanuts`:
+                console.log(`${count}` * 1.50);
+                break;
+        }
+    } else if (town === `Varna`) {
+        switch (product) {
+            case `coffee`:
+                console.log(`${count}` * 0.45);
+                break;
+            case `water`:
+                console.log(`${count}` * 0.70);
+                break;
+            case `beer`:
+                console.log(`${count}` * 1.10);
+                break;
+            case `sweets`:
+                console.log(`${count}` * 1.35);
+                break;
+            case `peanuts`:
+                console.log(`${count}` * 1.55);
+                break;
+        }
+    }
+}
+smallShop(["peanuts", "Varna", "2"]);
