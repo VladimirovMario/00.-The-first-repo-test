81b63a9ee9a6a8a399ab9fec3d1f70fc23a104a0
Mario <mariovladimirovfs@gmail.com>
2022-06-28 08:59:14 +0300

    upload

---
+++ 02. Lectures/07. For Loop - Lab/05. Character Sequence.js
@@ -0,0 +1,8 @@
+function characterSequence(input) {
+  let text = input[0];
+  let length = text.length;
+  for (let i = 0; i < length; i++) {
+    console.log(text[i]);
+  }
+}
+characterSequence(["Ice cream"]);
